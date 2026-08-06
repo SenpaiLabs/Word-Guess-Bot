3c65bf10e493617d47ea56d9101ffc318f0fa057
senpailabs <senpailabs@users.noreply.github.com>
2026-08-07 03:09:53 +0530

    fix: rename pyrogram session file to senpai.session

--- Senpai/core/bot.py
+++ Senpai/core/bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client
 from config import config
 
 app = Client(
-    name="word_guess_bot",
+    name="senpai",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
